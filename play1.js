115e04d5d8a63ea064f502771dad1fd288ae8a20
Dominique Masena <dominique.masena@live.ca>
2020-07-29 23:58:04 +0000

    play1.js

--- play1.js
+++ play1.js
@@ -1,33 +1,8 @@
-const { connect} = require('./client1');
-
-/**
- * Establishes connection with the game server
- */
-
-  // interpret incoming data as text
-
+const { connect } = require('./client1');
+const { setupInput } = require('./input');
 
 console.log('Connecting ...');
 connect();
 
-const setupInput = function(key) {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  
-  const handleUserInput = function(key) {
-    console.log(key);
-    if (key === '\u0003') {
-      process.exit(); 
-    }
-  }
-  stdin.on("data", handleUserInput);
-
-
-  // handleUserInput();
-  return stdin;
-
-}
-
 setupInput();
+
