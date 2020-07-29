89e4126dfbaab0e53386d11732ac84b9c9eae211
Dominique Masena <dominique.masena@live.ca>
2020-07-29 17:30:38 +0000

    play1.js

--- play1.js
+++ play1.js
@@ -1,22 +1,9 @@
-const net = require('net');
+const { connect } = require('./client1');
 
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '135.23.222.131',
-    port: 50541,
 
-  });
-  conn.setEncoding('utf8'); 
-  conn.on("data", function (message) {
-  
-    console.log(message);
-
-    });
-  return conn;
-  }
   // interpret incoming data as text
 
 
